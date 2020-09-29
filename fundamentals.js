b25bb988c0fa9f35bb35679756b708c6cc2bf881
Qing Sheng <xxxquillsq@gmail.com>
2020-09-29 14:11:42 +0100

    Dynamic properties

--- fundamentals.js
+++ fundamentals.js
@@ -6,9 +6,13 @@ const aCar = {
     registration :{year:'201',
                    countyCode:'WD',
                    number:'1058',
-                   } 
+                   },
+    mileage : '10000',
+    color:{exteriorColor:'red',
+           interiorFabric:{ texture :'leather',
+                            shade :'cream',
+           },
+    }
 };
 
-
-console.log(aCar.owner + ' drives a ' + aCar.type.make)
-console.log(' Reg.=' + aCar.registration.year+'-'+aCar.registration.countyCode+'-'+aCar.registration.number)
+console.log('It is a '+aCar.color.exteriorColor+ ' car, '+aCar.mileage+ 'mileage, with '+aCar.color.interiorFabric.texture+ ' interior.')
