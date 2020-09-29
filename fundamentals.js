b3ae85d2ee3a23d0c8c84039db693f039194642e
Qing Sheng <xxxquillsq@gmail.com>
2020-09-29 15:39:22 +0100

    Loop constructs

--- fundamentals.js
+++ fundamentals.js
@@ -7,6 +7,9 @@ const aCar = {
     type : {make:'Toyota ',
             model:'Corolla',
             CC:'1.8'},
+    features : ['Parking assist', 
+                'Alarm', 
+                'Tow-bar'],
     registration :{year:'201',
                    countyCode:'WD',
                    number:'1058',
@@ -19,5 +22,11 @@ const aCar = {
     }
 };
 
-console.log('First owner : ' + aCar.previous_owners[0].name+''+aCar.previous_owners[0].address)
-console.log('Second owner : ' + aCar.previous_owners[1].name+''+aCar.previous_owners[1].address)
+
+for (let i = 0 ; i < aCar.features.length ; i += 1) {  //features length3  012
+    console.log(aCar.features[i]) ;
+} 
+for (let i =0; i<aCar.previous_owners.length; i+=1 )
+{
+    console.log('previous owner'+(i+1)+':'+aCar.previous_owners[i].name+' '+aCar.previous_owners[i].address)
+}
